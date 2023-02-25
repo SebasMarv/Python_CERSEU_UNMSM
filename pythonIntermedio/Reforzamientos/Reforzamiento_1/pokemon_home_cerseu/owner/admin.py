75f076a27eb2f0a6fe57f7307a024c79b80d632f
Sebastian M <sebas.mar062699@gmail.com>
2023-02-25 11:34:21 -0500

    Avance en modificacion de plantillas y consultas a base de datos

--- pythonIntermedio/Reforzamientos/Reforzamiento_1/pokemon_home_cerseu/owner/admin.py
+++ pythonIntermedio/Reforzamientos/Reforzamiento_1/pokemon_home_cerseu/owner/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Owner
 
 # Register your models here.
-admin.site.register(Owner)
+# admin.site.register(Owner)
+
+@admin.register(Owner)
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ("id","nombre", "pais", "descripcion")
